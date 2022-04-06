4e48d4829c4f94512260d020e160348680922abd
Yaswanth Chunduru <54696560+yaswanth118@users.noreply.github.com>
2022-04-06 13:19:58 +0530

    Add files via upload

---
+++ Pythonnotebooks/L26-L27-L28-L29-L30-Lambda-Calculus-Encodings/init.ml
@@ -0,0 +1,6 @@
+#directory "/cs3100-2021/lectures/lambda";;
+#load_rec "/cs3100-2021/lectures/lambda/syntax.cmo";;
+#load_rec "/cs3100-2021/lectures/lambda/lambda_parse.cmo";;
+#load_rec "/cs3100-2021/lectures/lambda/eval.cmo";;
+
+open Syntax
